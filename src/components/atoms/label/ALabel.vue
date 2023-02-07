acd79324a131c6107dbca0c5ffb5ee0c03822d20
ecountergg <ecountergg@gmail.com>
2023-02-07 23:30:52 +0700

    feat(component): create component label

---
+++ src/components/atoms/label/ALabel.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import type { Props } from "./label.types";
+
+defineProps<Props>();
+</script>
+<template>
+	<label class="block text-gray-700 text-sm font-bold mb-2" :for="id">
+		{{ label }}
+	</label>
+</template>
---
+++ src/components/atoms/label/label.stories.ts
@@ -0,0 +1,23 @@
+import ALabel from "./ALabel.vue";
+import type { Meta, StoryFn } from "@storybook/vue3";
+import { ExtractComponentProps } from "@/types/components";
+
+const meta: Meta<ExtractComponentProps<typeof ALabel>> = {
+	title: "Atoms/Label",
+	component: ALabel,
+	args: {
+		id: "label",
+	},
+};
+
+export default meta;
+
+const Template: StoryFn<typeof ALabel> = (args) => ({
+	components: { ALabel },
+	setup() {
+		return { args };
+	},
+	template: `<ALabel v-bind="args" />`,
+});
+
+export const Default = Template.bind({});
